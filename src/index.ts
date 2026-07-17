1eb8c72f1e468345caa550e2ae8232cdd1dc6b38
Kevin <kevinlucas7910@gmail.com>
2026-07-16 21:25:00 -0400

    feat: adiciona servidor Mcwss

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-export {}
+import EventEmitter from 'events';
+import { Server } from 'socket-be';
+
+export class Mcwss extends EventEmitter<McbeWsServerEvents> {
+	readonly host: string;
+	readonly port: number;
+	readonly server: Server;
+	interval: NodeJS.Timeout | undefined;
+
+	constructor(host: string, port: number) {
+		super();
+		this.host = host;
+		this.port = port;
+		this.server = new Server({
+			port,
+			webSocketOptions: { host }
+		})
+	}
+
+	stop() {
+		this.server.stop();
+		if(this.interval) clearInterval(this.interval);
+	}
+}
+
+export type McbeWsServerEvents = {
+	start: [],
+	stop: [],
+	verifyMessages: [],
+	message: [string]
+}
